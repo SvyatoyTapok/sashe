02c0a8cc49c07af8b462138c97facfdfa10581a1
SvyatoyTapok <ewfbewrb@mail.ru>
2025-03-09 14:51:56 +0300

    admin popup + delete + add price

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,21 +1,39 @@
 "use client";
 import { useState } from 'react';
-import { useDeleteImageMutation, useGetAllGalleryQuery, useUploadImageMutation } from '../Store/pricesSlice';
+import { useDispatch } from 'react-redux';
+import {
+    useDeleteImageMutation,
+    useGetAllGalleryQuery,
+    useUploadImageMutation,
+    useGetPricesQuery,
+    useDeletePriceMutation
+} from '../Store/pricesSlice';
 import styles from './styles.module.css'
 import Image from 'next/image';
 import Link from "next/link";
+import AdminPopup from '../Components/AdminPopup/AdminPopup';
+import { openModal } from '../Store/modalSlice';
 
 export default function AdminPage() {
+
+    const dispatch = useDispatch();
+    const { data: prices = [] } = useGetPricesQuery("Маникюр");
+
     const [file] = useState<File | null>(null)
-    console.log(file)
     const logout = () => {
         document.cookie = "auth=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
         window.location.href = "/login";
     };
 
     const { data: images, error, isLoading } = useGetAllGalleryQuery({});
+    const [deletePrice] = useDeletePriceMutation();
     const [deleteImage] = useDeleteImageMutation();
     const [uploadImage] = useUploadImageMutation();
+    const handleDeletePrice = async (price: number) => {
+        await deletePrice(price)
+        window.location.reload();
+    }
+
     const handleChange = (e: File | null) => {
         if (e) {
             uploadImage(e).then(() => {
@@ -36,32 +54,44 @@ export default function AdminPage() {
         await deleteImage(image);
         window.location.reload();
     }
-    const handleShowImage = () => {
-
-    }
     return (
         <div className={styles.container}>
+            <AdminPopup />
             <h1 className={styles.mainText}>Добро пожаловать в админку!</h1>
+            <div>
+                <input onChange={(e) => handleChange(e.target.files ? e.target.files[0] : null)} accept="image/*" type="file" />
+                {file && <p>Выбран файл: {file.name}</p>}
+            </div>
             <div className={styles.imagesBlock}>
-                <div>
-                    <input onChange={(e) => handleChange(e.target.files ? e.target.files[0] : null)} accept="image/*" type="file" />
-                    {file && <p>Выбран файл: {file.name}</p>}
-                </div>
                 {imagesWithId.map((image: { id: number, image: string }) => (
                     <div key={image.id} className={styles.imageContainer}>
                         <Link target="_blank" href={`http://localhost:4300/images/gallery/${image.image}`}>
                             <Image
-                                onClick={handleShowImage}
                                 priority={true}
                                 src={`http://localhost:4300/images/gallery/${image.image}`}
                                 alt="image"
-                                width={400}
-                                height={400}
+                                width={240}
+                                height={240}
                             />
                         </Link>
                         <div onClick={() => handleDelete(image.image)} className={styles.deleteButton}>Удалить</div>
                     </div>
                 ))}
+            </div>
+
+            <div className={styles.bookingsContainer}>
+                {prices.map((priceItem: { id: number, name: string, cost: number }) => (
+                    <div
+                        className={styles.priceItem}
+                        key={priceItem.id}>
+                        {`${priceItem.name}, ${priceItem.cost}`}
+                        <div style={{ backgroundColor: "orange" }} className={styles.deleteButton}>Редактировать</div>
+                        <div onClick={() => handleDeletePrice(priceItem.id)} className={styles.deleteButton}>Удалить</div>
+                    </div>
+                ))}
+                <div onClick={() => dispatch(openModal())} style={{ backgroundColor: 'lightgreen', display: 'flex' }} className={styles.deleteButton}>
+                    Создать
+                </div>
             </div>
             <button onClick={logout} className={styles.button}>
                 Выйти
